00a8be3bd46b7bcbd96b231bb0f85a0b8301d307
SANAWULAI <60408947+Sanawulai@users.noreply.github.com>
2025-08-09 10:06:05 +0000

    update on get mapping

--- src/main/java/com/sanawulai/blogapplication/controller/PostController.java
+++ src/main/java/com/sanawulai/blogapplication/controller/PostController.java
@@ -4,10 +4,9 @@ import com.sanawulai.blogapplication.payload.PostDto;
 import com.sanawulai.blogapplication.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -26,4 +25,12 @@ public class PostController {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
 
     }
+
+    //get alll post rest api
+    @GetMapping
+    public List<PostDto> getAllPosts(){
+        return postService.getAllPosts();
+    }
+
+
 }
--- src/main/java/com/sanawulai/blogapplication/service/impl/PostServiceImpl.java
+++ src/main/java/com/sanawulai/blogapplication/service/impl/PostServiceImpl.java
@@ -7,6 +7,7 @@ import com.sanawulai.blogapplication.service.PostService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostServiceImpl implements PostService{
@@ -22,27 +23,42 @@ public class PostServiceImpl implements PostService{
     public PostDto createPost(PostDto postDto){
 
         //create dto to entity
-        Post post = new Post();
-        post.setTitle(postDto.getTitle());
-        post.setContent(postDto.getContent());
-        post.setDescription(postDto.getDescription());
-
-
-        Post saveNewPost = postRepository.save(post);
+       Post post = mapToEntity(postDto);
+        Post NewPost = postRepository.save(post);
 
 
         //convert entity to DTO
-        PostDto postResponse = new PostDto();
-        postResponse.setTitle(saveNewPost.getTitle());
-        postResponse.setId(saveNewPost.getId());
-        postResponse.setContent(saveNewPost.getContent());
-        postResponse.setDescription(saveNewPost.getDescription());
+        PostDto postResponse = mapToDTO(NewPost);
         return postResponse;
+
     }
 
     @Override
     public List<PostDto> getAllPosts() {
-        return List.of();
+
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(post ->
+                mapToDTO(post)).collect(Collectors.toList());
+    }
+
+    //convert entity to dto
+    private PostDto mapToDTO(Post post){
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setContent(post.getContent());
+        postDto.setDescription(post.getDescription());
+        return postDto;
+    }
+
+    //convert dto to entity
+    private Post mapToEntity(PostDto postDto){
+        Post post = new Post();
+        post.setId(postDto.getId());
+        post.setTitle(postDto.getTitle());
+        post.setContent(postDto.getContent());
+        post.setDescription(postDto.getDescription());
+        return post;
     }
 
 
